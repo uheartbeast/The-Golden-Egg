acad2600496b171827765a211d523daecba0a63a
Heartbeast <heartbeast.studios@gmail.com>
2022-05-18 10:23:25 -0600

    Added the fireblast spell

--- Battle.gd
+++ Battle.gd
@@ -5,11 +5,12 @@ const Blast = preload("res://Spells/Blast.tscn")
 
 func _ready():
 	for i in 20:
-		create_creature(Creature, Unit.ALLIANCE.FOE, preload("res://Creatures/RatStats.tres"))
+		create_creature(Creature, Unit.ALLIANCE.FOE, preload("res://Creatures/WolfStats.tres"))
 
 func create_creature(Creature, alliance, stats):
 	var creature = Creature.instance()
 	creature.alliance = alliance
+	
 	creature.stats = stats.duplicate()
 	add_child(creature)
 	creature.global_position = $Position2D.global_position + Vector2(rand_range(-16, 16), rand_range(-16, 16))
@@ -26,4 +27,10 @@ func _input(event):
 		blast.position = event.position
 		add_child(blast)
 		print("hi")
+
+func end_game():
+	get_tree().change_scene("res://Screens/DefeatScreen.tscn")
+
+func _on_Creature10_tree_exited():
+	call_deferred("end_game")
 	
--- Spells/Blast.gd
+++ Spells/Blast.gd
@@ -4,8 +4,9 @@ func _physics_process(delta):
 	var bodies = get_overlapping_bodies()
 	for body in bodies:
 		if not is_instance_valid(body): continue
-		body.apply_impulse(body.global_position, (body.global_position - global_position).normalized() * 500)
-	if not bodies.empty(): queue_free()
+		body.apply_impulse(body.global_position, (body.global_position - global_position).normalized() * 300)
+		body.stats.health -= 100
+	if not bodies.empty(): set_physics_process(false)
 
 
 func _on_Timer_timeout():
